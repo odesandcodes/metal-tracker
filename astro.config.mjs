7604b35c71f06abff8311b7369514f5148a4e20a
neps-influx <nathaniel.epstein@influxmarketing.com>
2026-01-30 10:13:33 -0500

    switch data source3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  // 1. Force Server-Side Rendering (updates on every visit)
-  output: 'server',
-  
-  // 2. Use the Cloudflare Adapter
+  // Revert to static (default)
+  output: 'static',
   adapter: cloudflare()
 });
