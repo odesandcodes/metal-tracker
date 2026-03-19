3dfc867214dd818b91019beb82123a9f57978900
neps-influx <nathaniel.epstein@influxmarketing.com>
2026-03-19 17:50:47 -0400

    clean up cf etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  // Revert to static (default)
   output: 'static',
-  adapter: cloudflare()
 });
